95412c0bf27cb44cc74bbcf0a40f7b03ddb5dec2
Giuseppe <giuseppe.mercurio@orbyta.it>
2021-01-14 17:37:36 +0100

    return to local polling

--- conversation_bot.py
+++ conversation_bot.py
@@ -382,9 +382,9 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    # updater.start_polling()
-    updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
-    updater.bot.setWebhook("https://blooming-citadel-68071.herokuapp.com/" + TOKEN)
+    updater.start_polling()
+    # updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
+    # updater.bot.setWebhook("https://blooming-citadel-68071.herokuapp.com/" + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
